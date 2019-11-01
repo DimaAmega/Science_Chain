7c68aef34ae9cc006e503a62b4699efb25a03b2c
DimaAmega <dima.amega1@gmail.com>
2019-11-01 12:16:09 +0300

    add Files V1.0

---
+++ getMultiplicators2.py
@@ -0,0 +1,53 @@
+import numpy as np
+import math as mt
+import mpmath as mp
+from scipy.integrate import odeint
+from scipy.linalg import eig as getEigenValues
+from InitCinditionSinphaseMode  import getInitCondition as IC
+def createPhi_s(X,L,G):
+        rside = createRsideOnePendulumsEquationMPMATH(L,G)
+        q = mp.odefun(rside,0,mp.matrix([0,X[0]]),tol=0.001,degree=4,method="taylor")
+        return lambda t:q(t)[0]
+def createQ0(N,i):
+    res = np.zeros(N)
+    res[i] = 1.0
+    return res
+def createRsideOnePendulumsEquationMPMATH(L,G):
+    return lambda t,q: [ q[1] , G-mp.sin(q[0]) -L*q[1] ] 
+def RSlinear(q,t,N,L,K,phi_s_mpf):
+    phi_s = float(phi_s_mpf(t))
+    X = np.zeros(2*N)
+    X[0] = q[1]
+    n = 0
+    X[1] = -L*q[1] - mt.cos(phi_s)*q[0] + K*(q[2] - q[0] ) 
+    while n < 2*N-4:
+        X[n+2] = q[n+3] 
+        X[n+3] = -L*q[n+3] - mt.cos(phi_s)*q[n+2] + K*( q[n+4] - 2*q[n+2] + q[n] )
+        n+=2
+    X[2*N-2] = q[2*N-1]
+    X[2*N-1] = -L*q[2*N-1] - mt.cos(phi_s)*q[2*N-2] + K*( q[2*N-4] - q[2*N-2] )
+    return X
+def createMatrixResiliense(t_end,N,L,K,phi_s):
+        res = np.zeros((2*N,2*N))
+        for i in range(2*N):
+            q = odeint(RSlinear, createQ0(2*N,i),np.arange(0,t_end + h_iter,h_iter),args=(N,L,K,phi_s),h0=h_iter,hmax=h_iter)
+            res[:,i] =  q[-1]
+        return res
+h_iter = 0.001
+######################
+##  IMPORT FUNCTION
+######################
+def getMultiplicators(phi_s,t_end,L,K,G,N):
+    res = createMatrixResiliense(t_end,N,L,K,phi_s)
+    EIG = getEigenValues(res)
+    return EIG[0]
+######################
+##  MAIN
+######################
+# X = IC(0.1,0.97)
+# phi_s = createPhi_s(X,0.1,0.97)
+# res  = getMultiplicators(phi_s,float(X[1]),0.1,0.21,0.97,6)
+# print(res)
+
+
+  
---
+++ mapData.py
@@ -0,0 +1,100 @@
+from multiprocessing import Pool
+import multiprocessing as mpproc
+from multiprocessing import Lock
+import numpy as np
+import sys
+import pickle
+from LineDataKRangeTools import *
+
+def calculateLineData(L,G,N,n_thread):
+    DATA = []
+    Count_iterations = 0
+    N_iterations = 1
+    ######################
+    ##      ОЦЕНКИ
+    ######################
+    x = createMatrix(N-1)
+    EIG = np.linalg.eig(x)
+    eigenValues = -1*EIG[0] # делаем все сч > 0
+    EVV = np.array(sorted(eigenValues,reverse=True))
+    Data = getRange(EVV,G,L)
+    h_k = 1.5
+    K_start = Data[0][0] - h_k
+    K_end = Data[-1][-1] + h_k
+    # print("Запустили поток L - {} Range K [{},{}]".format(L,K_start,K_end))
+    l1 = EVV[-1]
+    l2 = EVV[0]
+    ######################
+    ##      СЧЁТ
+    ######################
+    X = IC(L,G)
+
+    phi_s = createPhi_s(X,L,G)
+    t_period =float(X[1])
+    k_i = K_start
+    Prev = inCircle(getMul(phi_s,t_period,L,k_i,G,N))
+    Netx = Prev
+    step = getStep(k_i,K_start,K_end,l1,l2,G,L) 
+    k_i += step
+    while k_i < K_end:
+        multiplicators = getMul(phi_s,t_period,L,k_i,G,N)
+        Next = inCircle(multiplicators)
+        if Next*Prev<0:
+            if Prev>0:
+                res = binarySearch([k_i-step,k_i],0,phi_s,t_period,L,G,N)
+                DATA.append({"type":"Lboard","point":res})
+            else:
+                res = binarySearch([k_i-step,k_i],1,phi_s,t_period,L,G,N)
+                DATA.append({"type":"Rboard","point":res})
+        else:
+            DATA.append({"type":"point","point":k_i,"multiplicators":multiplicators})
+            if  Count_iterations % N_iterations == 0:
+                lock.acquire()
+                down(n_thread)
+                sys.stdout.write("thred - {}; L - {}; Progress - {}/100".format(n_thread,round(L,3),mt.floor((k_i - K_start)/(K_end - K_start)*100)))
+                up(n_thread)
+                lock.release()
+        Prev = Next
+        step = getStep(k_i,K_start,K_end,l1,l2,G,L)
+        k_i += step
+        Count_iterations+=1
+    lock.acquire()
+    down(n_thread)
+    sys.stdout.write("thred - {}; L - {}; Progress - 100/100".format(n_thread,L,mt.floor((k_i - K_start)/(K_end - K_start)*100)))
+    up(n_thread)
+    lock.release()
+    return {"Lambda":L,"Range_K":[K_start,K_end],"data":DATA}    
+
+#######################################
+###
+#######################################
+def init(l):
+    global lock
+    lock = l
+
+if __name__ == '__main__':
+    N_CPU = mpproc.cpu_count()
+    data = []
+    tasks = []
+    l = Lock()
+    print("START PROCESSES")
+    with Pool(processes=N_CPU,initializer=init, initargs=(l,)) as pool:
+        num_proc = 1
+        G = 0.97
+        N = 6
+        L_s = 0.8
+        L_e = 0.81
+        h_L = 0.01
+        L_arr = np.arange(L_s,L_e,h_L)
+        for L_i in L_arr:
+            tasks.append(pool.apply_async(calculateLineData,args = (L_i,G,N,num_proc),error_callback = lambda e: print(e)))
+            num_proc+=1
+        for task in tasks:
+            task.wait()
+            data.append(task.get())
+        pool.close()
+        pool.join()
+        down(num_proc)
+        with open('Count-{}.pickle'.format(N), 'wb') as f:
+            pickle.dump(data, f)
+        
